5d3a275ddb3fed8c3f36d7b7ef8e2cab29a6a984
William do Vale <63527052+William-vale@users.noreply.github.com>
2026-06-23 21:14:41 +0000

    primeiro commit do trabalho, criação index.html, css, js e um arquivo server.js

---
+++ js/script.js
@@ -0,0 +1,108 @@
+let botao = document.getElementById("buscar");
+
+botao.addEventListener("click", async () => {
+
+    let tema =
+        document.getElementById("pesquisa").value;
+
+    let resultados =
+        document.getElementById("resultados");
+
+    resultados.innerHTML =
+        "<p>Buscando artigos...</p>";
+
+    try {
+
+
+
+        let tema = document
+            .getElementById("pesquisa")
+            .value;
+
+        let pesquisaFinal =
+            `Capital Market ${tema}`;
+
+        let resposta = await fetch(
+            `https://api.openalex.org/works?search=${encodeURIComponent(pesquisaFinal)}&per_page=10`
+        );
+
+        let dados = await resposta.json();
+
+        dados.results.sort((a, b) => {
+
+            let anoA = a.publication_year || 0;
+            let anoB = b.publication_year || 0;
+
+            return anoB - anoA;
+
+        });
+
+        resultados.innerHTML = "";
+
+        dados.results.forEach(artigo => {
+
+            let titulo =
+                artigo.display_name || "Sem título";
+
+            let ano =
+                artigo.publication_year || "-";
+
+            let doi =
+                artigo.doi || "";
+
+            let pdf =
+                artigo.open_access?.oa_url || "";
+
+            let card = `
+
+            <div class="artigo">
+
+                <h3>${titulo}</h3>
+
+                <p>
+                    <strong>Ano:</strong>
+                    ${ano}
+                </p>
+
+            `;
+
+            if (doi) {
+
+                card += `
+                    <a
+                        href="${doi}"
+                        target="_blank"
+                        class="btn-artigo">
+
+                        Abrir Artigo
+                    </a>
+                `;
+            }
+
+            if (pdf) {
+
+                card += `
+                    <a
+                        href="${pdf}"
+                        target="_blank"
+                        class="btn-pdf">
+
+                        Ver PDF
+                    </a>
+                `;
+            }
+
+            card += `</div>`;
+
+            resultados.innerHTML += card;
+
+        });
+
+    } catch {
+
+        resultados.innerHTML =
+            "<p>Erro ao buscar artigos.</p>";
+
+    }
+
+});
---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+http.createServer((req, res) => {
+
+    let arquivo = req.url === "/"
+        ? "index.html"
+        : req.url;
+
+    let caminho = path.join(__dirname, arquivo);
+
+    let extensao =
+        path.extname(caminho);
+
+    let tipo = "text/html";
+
+    if (extensao === ".css") {
+        tipo = "text/css";
+    }
+
+    if (extensao === ".js") {
+        tipo = "application/javascript";
+    }
+
+    fs.readFile(caminho, (erro, conteudo) => {
+
+        if (erro) {
+            res.writeHead(404);
+            return res.end("Arquivo nao encontrado");
+        }
+
+        res.writeHead(200, {
+            "Content-Type": tipo
+        });
+
+        res.end(conteudo);
+    });
+
+}).listen(3000, () => {
+
+    console.log("Servidor iniciado");
+    console.log("http://localhost:3000");
+
+});
+``
